06d39a60643686e3845ccb68145459a52a84474e
Mengzhen <gao.mengzhen94@gmail.com>
2017-02-21 16:13:31 -0500

    delete useless comment

--- app.js
+++ app.js
@@ -11,24 +11,3 @@ sampleApp.config(['$routeProvider', function($routeProvider){
 sampleApp.controller('ShowOrderController', function($scope, $routeParams){
 	$scope.order_id = $routeParams.orderId;
 });
-
-/*
-var sampleApp = angular.module('sampleApp', []);
-
-sampleApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/ShowOrder/:orderId', {
-	templateUrl: 'templates/show_order.html',
-	controller: 'ShowOrderController'
-      });
-}]);
-
-
-sampleApp.controller('ShowOrderController', function($scope, $routeParams) {
-
-	
-	$scope.order_id = $routeParams.orderId;
-
-});
-*/
